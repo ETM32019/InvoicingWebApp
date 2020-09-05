7ecc810def81dc90dd1a819452a9d5a3c1ef1d95
Landon McKell <landon.roney7923@gmail.com>
2020-09-05 00:53:49 -0600

    remodelled invoice model, started on invoice api route

--- models/Invoice.js
+++ models/Invoice.js
@@ -6,36 +6,73 @@ const InvoiceSchema = new Schema({
     user: {
       type: Schema.Types.ObjectId
     },
+    title: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String
+    },
+    address: {
+      type: String
+    },
+    phone: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return /\d{3}-\d{3}-\d{4}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: [true, "User phone number required"]
+    },
+    businessnumber: {
+      type: String
+    },
+    invoicenumber: {
+      type: String
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    terms: {
+      type: String
+    },
+    itemdescription: {
+      type: String,
+      required: true
+    },
+    itemrate: {
+      type: String,
+      required: true
+    },
+    itemamount: {
+      type: mongoose.Types.Decimal128
+    },
+    itemtaxable: {
+      type: Boolean,
+      default: false
+    },
+    quantity: {
+      type: Number,
+      default: 1
+    },
+    additionaldetails: {
+      type: String
+    },
     notes: {
       type: String
     },
     previewemail: {
       type: String
     },
-    discount: {
-      type: {
-        type: String
-      },
-      percent: {
-        type: mongoose.Types.Decimal128
-      }
-    },
-    recordpayment: {
-      amount: {
-        type: Number,
-        required: true
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      },
-      method: {
-        type: String,
-        required: true
-      },
-      notes: {
-        type: String
-      }
+    color: {
+      type: String
     }
   }
 });
--- routes/api/invoice.js
+++ routes/api/invoice.js
@@ -1,10 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/invoice
-// @desc    test route
-// @access  Public
+const Invoice = require("../../models/Invoice");
+const User = require("../../models/User");
 
-router.get("/", (req, res) => res.send("Invoice Route"));
+// @route   POST api/invoices
+// @desc    Create Invoice
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    check("title", "Title is required")
+      .not()
+      .isEmpty(),
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("itemdescription", "Item description is required")
+      .not()
+      .isEmpty(),
+    check("itemrate", "Item rate is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    // check validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      // get current user
+      const user = await User.findById(req.user.id).select("-password");
+
+      // create new invoice object
+    //   const newInvoice = new Invoice({
+
+    //   })
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
